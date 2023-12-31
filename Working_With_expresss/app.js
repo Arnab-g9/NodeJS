981e92f371be4cba938a2251c53583c4a1156eff
Arnabg_9 <arnabghorai15@gmail.com>
2023-12-31 23:04:26 +0530

    initial commit

--- Working_With_expresss/app.js
+++ Working_With_expresss/app.js
@@ -1,6 +1,14 @@
 const http = require("http");
 const express = require("express");
 const app = express();
-app.listen(3000, () => {
-  console.log("server running on port 3000");
+app.use((req, res, next) => {
+  console.log("In the middleWare!");
+  next(); // -- this allowes the request to continue to the next middleware in line, if we don't call the next then we shoud send the response otherwise the request will die.
+}); /**  -- use allowes us to use new middleware function, It accepts array of request handlers. use method accept the callback in callback it accept the next method as a parameter which is provide by the express. */
+
+app.use((req, res, next) => {
+  console.log("In the another middleWare!");
 });
+
+const server = http.createServer(app);
+app.listen(3000);
