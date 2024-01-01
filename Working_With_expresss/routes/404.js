5692a2a25d91a4eb3d326c4c8b87c51b110127e0
Arnabg_9 <arnabghorai15@gmail.com>
2024-01-01 22:02:38 +0530

    intitail commit

--- Working_With_expresss/routes/404.js
+++ Working_With_expresss/routes/404.js
@@ -3,7 +3,11 @@ const express = require("express");
 const router = express.Router();
 
 router.use((req, res, next) => {
-  res.status(404).send("<h1>Page not found</h1>");
+  res
+    .status(404)
+    .send(
+      "<h1>Page not found</h1>"
+    ); /** this type of one by one method is called method chaining. As we can send the status code and remember the send method will always be at last. we can set the header also */
 });
 
 module.exports = router;
