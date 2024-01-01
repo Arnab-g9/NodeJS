e32c3ff57aec1e3c66ef4bfa6ad729ce55e920cb
Arnabg_9 <arnabghorai15@gmail.com>
2024-01-01 22:33:36 +0530

    initial commit

--- Working_With_expresss/app.js
+++ Working_With_expresss/app.js
@@ -19,6 +19,7 @@ app.use("/", (req, res, next) => {
 });
 
 app.use(
+  "/admin",
   adminRoutes
 ); /** importing adminRoute object that contains the admin related routes */
 
--- Working_With_expresss/routes/admin.js
+++ Working_With_expresss/routes/admin.js
@@ -3,14 +3,17 @@ const express = require("express");
 const router =
   express.Router(); /** this router method comes with express. It's like mini express app or plugable with othe express app, which we can export to the other file */
 
+//  /admin/add-product => GET
 router.get("/add-product", (req, res, next) => {
   // console.log("In the add-product middleWare!");
   res.send(
-    '<form action="/product" method="POST"> <input type="text" name="title"> <input type="number" name="age"> <button type="submit">Add Product</button> </form>'
+    '<form action="/admin/add-product" method="POST"> <input type="text" name="title"> <input type="number" name="age"> <button type="submit">Add Product</button> </form>'
   );
 });
 
-router.post("/product", (req, res, next) => {
+// /admin/add-product => POSt
+router.post("/add-product", (req, res, next) => {
+  /** path name can be same as the method are different like get and post */
   console.log(req.body);
   // console.log(req.body.title);
   // console.log(req.body.age);
