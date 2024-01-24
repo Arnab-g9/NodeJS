575f8bbb1607b51e44db23f5cf4a05490b40b502
Arnabg_9 <arnabghorai15@gmail.com>
2024-01-24 14:21:55 +0530

    init

--- Week-2-Enhanceing The App/controllers/shop.js
+++ Week-2-Enhanceing The App/controllers/shop.js
@@ -22,7 +22,14 @@ exports.getIndex = (req, res, next) => {
 exports.getCart = (req, res, next) => {
   res.render("shop/cart", {
     path: "/cart",
-    pageTitle: "Your cart",
+    pageTitle: "Your Cart",
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders",
   });
 };
 
