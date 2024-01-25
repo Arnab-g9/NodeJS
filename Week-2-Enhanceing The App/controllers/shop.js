13c438d92b9b12f0143e85ba533bbcc6599750ed
Arnabg_9 <arnabghorai15@gmail.com>
2024-01-25 21:46:36 +0530

    init

--- Week-2-Enhanceing The App/controllers/shop.js
+++ Week-2-Enhanceing The App/controllers/shop.js
@@ -1,4 +1,6 @@
 const Product = require("../models/product"); //importing product class
+const Cart = require("../models/cart"); //importing cart model
+
 exports.getProducts = (req, res, next) => {
   Product.fetchAll((products) => {
     res.render("shop/product-list", {
@@ -7,6 +9,20 @@ exports.getProducts = (req, res, next) => {
       path: "/products",
     });
   }); // get product list by static method of fetch all of product controller
+};
+
+exports.getProduct = (req, res, next) => {
+  // console.log(req);
+  const prodId = req.params.productId; //request.params helps to access the parameter of the request/url like here product id
+  // console.log(prodId);
+  Product.findById(prodId, (product) => {
+    //console.log(product);
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: "product.title",
+      path: "/products",
+    });
+  });
 };
 
 exports.getIndex = (req, res, next) => {
@@ -24,6 +40,15 @@ exports.getCart = (req, res, next) => {
     path: "/cart",
     pageTitle: "Your Cart",
   });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  // console.log(prodId);
+  Product.findById(prodId, (product) => {
+    Cart.addProduct(prodId, product.price);
+  });
+  res.redirect("/cart");
 };
 
 exports.getOrders = (req, res, next) => {
--- Week-2-Enhanceing The App/routes/shop.js
+++ Week-2-Enhanceing The App/routes/shop.js
@@ -5,7 +5,10 @@ const shopController = require("../controllers/shop");
 
 router.get("/", shopController.getIndex);
 router.get("/products", shopController.getProducts);
+router.get("/products/:productId", shopController.getProduct);
+/** here /products/:productId is dynamic where /products is a static path or route but after/:productId is variable part of different request through which we can extract the information of the products */
 router.get("/cart", shopController.getCart);
+router.post("/cart", shopController.postCart);
 router.get("/orders", shopController.getOrders);
 router.get("/checkout", shopController.getCheckout);
 
